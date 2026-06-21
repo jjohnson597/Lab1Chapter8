c3cc3794a474f78e4d7328a4e97047c32f5b7677
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-21 12:39:23 -0400

    Create: Add rectangle area and perimeter functions

---
+++ rectangle.py
@@ -0,0 +1,15 @@
+"""
+Program: rectangle.py
+Author: Jaylen Johnson
+Purpose: Provides functions to calculate the area and perimeter of a rectangle.
+Starter code: No starter code was used.
+Date: 6/21/2026
+"""
+
+
+def calc_area(width, height):
+    return width * height
+
+
+def calc_perimeter(width, height):
+    return 2 * (width + height)
