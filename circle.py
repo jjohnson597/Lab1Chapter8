f6bd5b3502ca04716a11ad1e991b9d52ff169833
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-21 12:25:37 -0400

    Added header and math import

---
+++ circle.py
@@ -0,0 +1,9 @@
+"""
+Program: circle.py
+Author: Jaylen Johnson
+Purpose: Provides functions to calculate the area and circumference of a circle.
+Starter code: No starter code was used.
+Date: 6/21/2026
+"""
+
+import math
