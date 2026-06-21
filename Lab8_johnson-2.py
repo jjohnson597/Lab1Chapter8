1b78528eb099a58cc6cdf07a576d41850e2ab31a
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-21 12:51:47 -0400

    Add: Import geometry modules using aliases

--- Lab8_johnson-2.py
+++ Lab8_johnson-2.py
@@ -5,3 +5,9 @@ Purpose: Geometry Calculator that calculates circle and rectangle measurements u
 Starter code: No starter code was used.
 Date: 6/21/2026
 """
+
+import circle as c
+import rectangle as r
+
+c.calc_area()
+r.calc_area()
