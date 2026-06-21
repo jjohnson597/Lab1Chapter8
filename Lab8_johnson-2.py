27da4fbd1b30b73fdadd967f464a31097dc23c97
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-21 12:45:25 -0400

    Create: Add main program header

---
+++ Lab8_johnson-2.py
@@ -0,0 +1,7 @@
+"""
+Program: Lab8_johnson-2.py
+Author: Jaylen Johnson
+Purpose: Geometry Calculator that calculates circle and rectangle measurements using imported modules.
+Starter code: No starter code was used.
+Date: 6/21/2026
+"""
